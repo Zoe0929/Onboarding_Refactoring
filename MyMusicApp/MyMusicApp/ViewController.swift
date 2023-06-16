df273a53bf7b9e806495ab496787ddf7984f7c8b
Zoe <Zoe.urssu@gmail.com>
2023-06-16 16:14:04 +0900

    [set⚙️] #1 프로젝트 초기 세팅

---
+++ MyMusicApp/MyMusicApp/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  MyMusicApp
+//
+//  Created by 지희의 MAC on 2023/06/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+    }
+    
+    private func setUI(){
+        setViewHierarchy()
+        setConstraints()
+        view.backgroundColor = .white
+    }
+    
+    private func setViewHierarchy() {
+        
+    }
+    
+    private func setConstraints() {
+        
+    }
+
+
+}
+
